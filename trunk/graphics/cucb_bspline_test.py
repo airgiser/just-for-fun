16b5064621f97c730af1950d8983d65e6c4bb24f
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2011-11-28 06:37:19 +0000

    Add b-spline test code(python)

--- trunk/graphics/cucb_bspline_test.py
+++ trunk/graphics/cucb_bspline_test.py
@@ -8,12 +8,12 @@ class GFrame(wx.Frame):
 	def __init__(self, parent, title):
 		super(GFrame, self).__init__(parent, title=title, size=(400,300))
 
-		wx.FutureCall(2000, self.DrawLine)
+		wx.FutureCall(2000, self.Draw)
 
 		self.Centre()
 		self.Show()
 	
-	def DrawLine(self):
+	def Draw(self):
 
 		size = 4;
 		points = gl.points_create(size)
@@ -30,16 +30,30 @@ class GFrame(wx.Frame):
 		pt4.x = 300
 		pt4.y = 100
 
-		spline_size = 9
-		spline = gl.points_create(spline_size)
-		gl.cubic_b_spline_interp(pt1, pt2, pt3, pt4, spline, spline_size)
-		
 		gl.points_set_by_index(points, size, 0, pt1)
 		gl.points_set_by_index(points, size, 1, pt2)
 		gl.points_set_by_index(points, size, 2, pt3)
 		gl.points_set_by_index(points, size, 3, pt4)
-		self.DrawPline(points, size)
+		#self.DrawPline(points, size)
+
 		print 'spline:'
+		spline_size = 9
+		spline = gl.points_create(spline_size)
+
+		#draw begin
+		gl.cubic_b_spline_interp(pt1, pt1, pt1, pt2, spline, spline_size)
+		self.DrawPline(spline, spline_size)
+		gl.cubic_b_spline_interp(pt1, pt1, pt2, pt3, spline, spline_size)
+		self.DrawPline(spline, spline_size)
+
+		#draw spline
+		gl.cubic_b_spline_interp(pt1, pt2, pt3, pt4, spline, spline_size)
+		self.DrawPline(spline, spline_size)
+
+		#draw end
+		gl.cubic_b_spline_interp(pt2, pt3, pt4, pt4, spline, spline_size)
+		self.DrawPline(spline, spline_size)
+		gl.cubic_b_spline_interp(pt3, pt4, pt4, pt4, spline, spline_size)
 		self.DrawPline(spline, spline_size)
 
 	def DrawPline(self, points, size):
