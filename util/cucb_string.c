599921c3b61b1043eb6564d183d70ffae64ccad7
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2011-12-04 05:27:54 +0000

    Add test code for mb_to_wchar and wchar_to_mb string utilities

--- util/cucb_string.c
+++ util/cucb_string.c
@@ -4,24 +4,57 @@
 #include <windows.h>
 #endif
 
-size_t wchar_to_mb(char *dest, const wchar_t *src, size_t maximum)
+size_t mb_to_wchar(wchar_t *dest, const char *src, size_t maximum)
 {
-#if defined(WIN32) || defined(WINCE)
-	int count = WideCharToMultiByte(/*CP_ACP*/936, 0, src, -1, dest, maximum, NULL, NULL);
-	dest[count] = 0;
-	return count;
-#else
-	return wcstombs(dest, src, maximum);
-#endif
+	return mbstowcs(dest, src, maximum);
 }
 
-size_t mb_to_wchar(wchar_t *dest, const char *src, size_t maximum)
+size_t wchar_to_mb(char *dest, const wchar_t *src, size_t maximum)
+{
+	return wcstombs(dest, src, maximum);
+}
+
+size_t ansi_to_unicode(wchar_t *dest, const char *src, size_t maximum)
 {
 #if defined(WIN32) || defined(WINCE)
 	int count = MultiByteToWideChar(/*CP_ACP*/936, 0, src, -1, dest, maximum);
 	dest[count] = 0;
 	return count;
-#else
-	return mbstowcs(dest, src, maximum);
+#elif defined(LINUX) || defined(UNIX)
+	return 0;
+#endif
+}
+
+size_t unicode_to_ansi(char *dest, const wchar_t *src, size_t maximum)
+{
+#if defined(WIN32) || defined(WINCE)
+	int count = WideCharToMultiByte(/*CP_ACP*/936, 0, src, -1, dest, maximum, NULL, NULL);
+	dest[count] = 0;
+	return count;
+#elif defined(LINUX) || defined(UNIX)
+	return 0;
+#endif
+}
+
+size_t utf8_to_unicode(wchar_t *dest, const char *src, size_t maximum)
+{
+#if defined(WIN32) || defined(WINCE)
+	int count = MultiByteToWideChar(CP_UTF8, 0, src, -1, dest, maximum);
+	dest[count] = 0;
+	return count;
+#elif defined(LINUX) || defined(UNIX)
+	return 0;
+#endif
+}
+
+size_t unicode_to_utf8(char *dest, wchar_t *src, size_t maximum)
+{
+#if defined(WIN32) || defined(WINCE)
+	int count = WideCharToMultiByte(CP_UTF8, 0, src, -1, dest, maximum,
+			NULL, NULL);
+	dest[count] = 0;
+	return count;
+#elif defined(LINUX) || defined(UNIX)
+	return 0;
 #endif
 }
--- util/cucb_string_test.c
+++ util/cucb_string_test.c
@@ -1,6 +1,27 @@
 #include "cucb_string.h"
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <assert.h>
 
+
+#define MAX_LEN 255
 int main(int argc, char *argv[])
 {
+	//setlocale
+	
+	char str[MAX_LEN];
+	wchar_t w_str[MAX_LEN];
+
+	sprintf(str, "%s", "hello!");
+	wprintf(L"ansi:%s\n", str);
+
+	//swprintf(w_str, MAX_LEN, L"%s", str);
+	mb_to_wchar(w_str, str, MAX_LEN);
+	wprintf(L"unicode:%S\n", w_str);
+
+	//sprintf(str, "%S", w_str);
+	wchar_to_mb(str, w_str, MAX_LEN);
+	wprintf(L"ansi:%s\n", str);
 	return 0;
 }
