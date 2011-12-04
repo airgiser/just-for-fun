0710b9f897ff8f404f7ad8e809ce62ee694719a4
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2011-12-04 05:27:54 +0000

    Add test code for mb_to_wchar and wchar_to_mb string utilities

--- trunk/util/cucb_string.h
+++ trunk/util/cucb_string.h
@@ -7,24 +7,6 @@
 CUCB_BEGIN
 
 /*
- * \brief Convert a wide-character string to a multibyte string
- *
- * \param dest
- * 	Pointer to an array of char elements at least max bytes long. 
- * \param src
- * 	The wide-character string to be translated.
- * \param maximum
- * 	Maximum number of bytes to be written to dest.
- *
- * \return size_t
- * 	The number of bytes(not characters) translated and written to dest,
- * 	not including the ending null-character.
- * 	If an invalid multibyte character is encountered,
- * 	a -1 value is returned.
- */
-size_t wchar_to_mb(char *dest, const wchar_t *src, size_t maximum);
-
-/*
  * \brief Convert a multibyte string to a wide-character string
  *
  * \param dest
@@ -42,6 +24,33 @@ size_t wchar_to_mb(char *dest, const wchar_t *src, size_t maximum);
  * 	a -1 value is returned.
  */
 size_t mb_to_wchar(wchar_t *dest, const char *src, size_t maximum);
+
+/*
+ * \brief Convert a wide-character string to a multibyte string
+ *
+ * \param dest
+ * 	Pointer to an array of char elements at least max bytes long. 
+ * \param src
+ * 	The wide-character string to be translated.
+ * \param maximum
+ * 	Maximum number of bytes to be written to dest.
+ *
+ * \return size_t
+ * 	The number of bytes(not characters) translated and written to dest,
+ * 	not including the ending null-character.
+ * 	If an invalid multibyte character is encountered,
+ * 	a -1 value is returned.
+ */
+size_t wchar_to_mb(char *dest, const wchar_t *src, size_t maximum);
+
+
+size_t ansi_to_unicode(wchar_t *dest, const char *src, size_t maximum);
+
+size_t unicode_to_ansi(char *dest, const wchar_t *src, size_t maximum);
+
+size_t utf8_to_unicode(wchar_t *dest, const char *src, size_t maximum);
+
+size_t unicode_to_utf8(char *dest, wchar_t *src, size_t maximum);
 
 CUCB_END
 
