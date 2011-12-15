73ca380c29060048eed7e02e9d40761ef1d2fd1a
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2011-12-15 09:50:34 +0000

    Add cpp example.

---
+++ examples/cpp/thinking_in_c++_vol1/stream/format.cpp
@@ -0,0 +1,21 @@
+/*
+ * \brief Output formating 
+ * \author airfox
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Number formatting in decimal,octal and hexadecimal.
+	cout<<"This is a number: "<<15<<endl;
+	cout<<"in decimal: "<<dec<<15<<endl;
+	cout<<"in octal: "<<oct<<15<<endl;
+	cout<<"in hex: "<<hex<<15<<endl;
+
+	cout<<"This is a floating-point number: "<<3.14159<<endl;
+	cout<<"This is non-printing char (escape) : "<<char(27)<<endl;
+
+	return 0;
+}
