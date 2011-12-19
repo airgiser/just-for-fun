cc2cc1c2d6c4fa122a774a845dbad906affadbe6
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2011-12-19 03:42:44 +0000

    Add cpp demo(bit oper)

---
+++ examples/cpp/thinking_in_c++_vol1/1.intro/vectorintro.cpp
@@ -0,0 +1,53 @@
+/*
+ * \brief Copy an entire file into a vector of string
+ * \author airfox
+ */
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+static void print_content()
+{
+	vector<string> vec;
+	ifstream fin("Makefile");
+	string str;
+	while(getline(fin, str))
+	{
+		vec.push_back(str);
+	}
+
+	int i = 0;
+	for(i = 0; i < vec.size(); i++)
+	{
+		cout<<"Line"<<i<<": "<<vec[i]<<endl;
+	}
+}
+
+static void print_words()
+{
+	vector<string> words;
+	ifstream fin("Makefile");
+	
+	string word;
+	while(fin>>word)
+	{
+		words.push_back(word);
+	}
+
+	int i = 0;
+	for(i = 0; i < words.size(); i++)
+	{
+		cout<<words[i]<<endl;
+	}
+}
+
+int main()
+{
+	print_content();
+	cout<<endl;
+	print_words();
+
+	return 0;
+}
