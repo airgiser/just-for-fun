a9bc17989429eb6a6028681345da47e5e03f9a95
airgis <airgis@3d45adca-298f-4720-94e4-73bc880cdfd0>
2012-02-10 09:29:11 +0000

    Add c# example WebBrowser

---
+++ trunk/examples/cs/WebBrowser/WebBrowser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class WebBrowser : Form
+    {
+        public WebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            webBrowserForm.Navigate(new Uri(address.SelectedItem.ToString()));
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowserForm.GoHome();
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowserForm.GoBack();
+        }
+
+        private void forwardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowserForm.GoForward();
+        }
+
+        private void WebBrowser_Load(object sender, EventArgs e)
+        {
+            address.SelectedIndex = 0;
+            webBrowserForm.GoHome();
+        }
+    }
+}
